14493836d09d8f979d6da5885d1e3ac60b7bd277
iplusplus812 <iplusplus@yeah.net>
2014-08-25 23:14:41 +0800

    why lose

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,56 @@
-window.onload = function(){
-	var messages = [];
-	var socket = io.connect("http://localhost:3700");
-	var field = document.getElementById("field");
-	var sendButton = document.getElementById("send");
-	var content = document.getElementById("content");
+var app = {
+	socket: {},
+	initialize: function(){
 
-	socket.on("message",function(data){
+		_.bindAll(this,"onMessage","sendMessage","keyupHandler");
+		
+		this.socket = io.connect("http://localhost:3700");
+		this.field = $("#field");
+		this.sendButton = $("#send");
+		this.content = $("#content");
+		this.name = $("#name");
+
+		this.socket.on("message",this.onMessage,this);
+
+		this.sendButton.click(this.sendMessage);
+		this.field.keyup(this.keyupHandler);
+		
+	},
+	keyupHandler: function(e){
+		if(e.keyCode == 13){
+				this.sendMessage();
+		}
+	},
+	sendMessage: function(){
+
+		var username = this.name.val();
+		if(username == ""){
+			alert("Please input your name!");
+		} else {
+			var text = this.field.val();
+
+			this.socket.emit("send",{message: text,username: username});
+			this.field.val("");
+		}
+	},
+	onMessage: function(data){
+
 		if(data.message){
-			messages.push(data.message);
+			
 			var html = "";
-			for(var i= 0; i <messages.length;i++){
-				html += messages[i] + "<br />";
-			}
-			content.innerHTML = html;
+			
+			html += "<b>"+(data.username?data.username:"server")+":</b>";
+			html += data.message + "<br />";
+
+			
+			this.content.append(html);
+			this.content.scrollTop(this.content.height());
 		} else {
 			console.log("there is an error happen:",data);
 		}
-	});
-
-	sendButton.onclick = function(){
-		var text = field.value;
-
-		socket.emit("send",{message: text});
 	}
 }
+
+$(function(){
+	app.initialize();
+});
