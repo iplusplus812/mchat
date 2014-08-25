3b2340289f1a5d8cf0003351115decae88903684
iplusplus812 <iplusplus@yeah.net>
2014-08-25 23:27:43 +0800

    add

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ var express = require("express");
 var app = express();
 var port  = 3700;
 
+app.set("views",__dirname+"/tpl");
+app.set("view engine", "jade");
+app.engine("jade", require("jade").__express);
+app.use(express.static(__dirname + "/public"));
+
 app.get("/",function(req,res){
-	res.send("It works!");
+	res.render("page");
 });
 
-app.listen(port);
+var io = require("socket.io").listen(app.listen(port));
 console.log("listening on port " + port);
+
+io.sockets.on("connection",function(socket){
+	socket.emit("message",{message:"welcome to the chat..."});
+	socket.on("send",function(data){
+		io.sockets.emit("message",data);
+	});
+});
